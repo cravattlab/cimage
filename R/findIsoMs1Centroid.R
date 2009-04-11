e0f4004ae60c38a6358f6a0e47051c917ad190a7
chu <chu@691117a8-ef3d-11dd-b559-41d664898b09>
2009-04-11 01:31:38 +0000

    1. fix a bug in peak pair detection, do not save a hit unless it has one peak from current retention time. This is to make sure that all peak pairs from the same envelope are indentified together. 2. various filters to remove false positive peaks, such as check isotopic distribution, background noise cutoff etc.

--- R/findIsoMs1Centroid.R
+++ R/findIsoMs1Centroid.R
@@ -1,6 +1,6 @@
 library(xcms)
 
-memory.limit(2560) # 2.5G memory maximum
+#memory.limit(2560) # 2.5G memory maximum
 
 input.file <- "bash.input.file"
 
@@ -38,15 +38,26 @@ filename <- paste(input.path, "/", input.file.base, input.file.suffix, sep="")
 xfile <- xcmsRaw( filename, profstep=0 )
 
 xpeaks <- findPeaks(xfile,method="centWave", ppm=5, snthresh=2.0, minptsperpeak=2)
+                                        # sort the matrix by retention time first
+xpeaks.save <- xpeaks
+xpeaks <- xpeaks[order(xpeaks[,"rt"]), ]
+
+                                        # label rownames
 num.peaks <- dim(xpeaks)[1]
 rownames(xpeaks) <- seq(1, num.peaks)
-                                        # sort the matrix by retention time first
-xpeaks <- xpeaks[order(xpeaks[,"rt"]), ]
                                         # make a fresh empty list
 n.nonredundant.hit <- 0
-pair.list <- NULL
+pair.list <- data.frame(idx1=numeric(0),idx2=numeric(0),charge=numeric(0),
+                        mz1=numeric(0), mz2=numeric(0),
+                        rt1=numeric(0), rt2=numeric(0), hit.id=numeric(0) )
 
-for ( rt in levels(factor(xpeaks[,"rt"])) ) {
+rt.list <- levels(factor(xpeaks[,"rt"]))
+cat( paste("detect isotopic peak pairs in", length(rt.list), "rt windows in total\n") )
+cat( "Finished " )
+for ( rt.i in 1:length(rt.list) ) {
+  cat(".")
+  if (rt.i%%100 == 0) cat(rt.i)
+  rt <- rt.list[rt.i]
   rt.range <- c( as.numeric(rt)-rt.cut, as.numeric(rt)+rt.cut)
   this.rt <- (xpeaks[,"rt"] > rt.range[1]) & (xpeaks[,"rt"] < rt.range[2] )
   #this.rt <- factor(xpeaks[,"rt"])==rt
@@ -60,6 +71,11 @@ for ( rt in levels(factor(xpeaks[,"rt"])) ) {
                      dimnames=list(names(mz.this.rt), names(mz.this.rt) ) )
   mz.delta.ppm <- mz.delta # tolerance matrix
 
+  # for redundancy check
+  # pair.list.prev.rt contains all existing pairs
+  # which could potentially overlap with the current rt window
+  pair.list.prev.rt <- pair.list[ (pair.list[,"rt1"] >= rt.range[1] | pair.list[,"rt2"] >= rt.range[1] ), ]
+
   for (i in 1:num.mz.this.rt ) {
     for ( j in i:num.mz.this.rt ) {
       mz.delta[i,j] <- mz.delta[j,i] <- abs(mz.this.rt[i] - mz.this.rt[j])
@@ -67,7 +83,8 @@ for ( rt in levels(factor(xpeaks[,"rt"])) ) {
     }
   }
   for (charge in charge.states) {
-    pair.list.this.charge <- NULL
+    pair.list.this.charge <- pair.list[F,] # an empty dataframe tracking pairs within this rt and charge
+    n.hit.this.charge <- 0
     pair.mz.delta <- pair.mass.delta / charge
     mz.delta.pass <- abs(mz.delta - pair.mz.delta) < mz.delta.ppm
     for (i in 1:num.mz.this.rt ) {
@@ -88,81 +105,129 @@ for ( rt in levels(factor(xpeaks[,"rt"])) ) {
                                         # intensity
           peak.maxo1 <- xpeaks[peak.index1, intensity.tag]
           peak.maxo2 <- xpeaks[peak.index2, intensity.tag]
+                                        # retension time
+          peak.rt1 <- xpeaks[peak.index1, "rt"]
+          peak.rt2 <- xpeaks[peak.index2, "rt"]
                                         # check peak intensity ratio to fall in certain range
           ratio <- peak.maxo1 / peak.maxo2
           if ( ratio < intensity.ratio.range[1] | ratio > intensity.ratio.range[2] ) next
-          new.pair <- c( peak.index1, peak.index2, charge, "0" ) # last number is tracking nonredudant hit
-          if ( length(pair.list.this.charge) == 0 ) {
-            n.nonredundant.hit <- n.nonredundant.hit + 1
-            new.pair[4] <- n.nonredundant.hit
-            pair.list.this.charge <- new.pair
-          } else {
-            for ( k in seq(1,length(pair.list.this.charge),by=4) ) {
-              tmp.mz1 <- xpeaks[ pair.list.this.charge[k], "mz" ]
-              tmp.mz2 <- xpeaks[ pair.list.this.charge[k+1], "mz" ]
-              if ( ((tmp.mz2>peak.mz1)&(tmp.mz2<peak.mz2)) | ((tmp.mz1>peak.mz1)&(tmp.mz1<peak.mz2)) ) {
-                                        # this pair is from an existing isotopic cluster
-                new.pair[4] <- pair.list.this.charge[k+3]
+
+          # check whether the exact peak pair has been found in previous rt window before
+          redundant <- FALSE
+          if ( dim(pair.list.prev.rt)[1] > 0 ) {
+            for (k in 1:dim(pair.list.prev.rt)[1] ) {
+              if ((pair.list.prev.rt[k,"idx1"] == peak.index1) &&
+                  (pair.list.prev.rt[k,"idx2"] == peak.index2) &&
+                  (pair.list.prev.rt[k,"charge"] == charge ) ) {
+                redundant <- TRUE
                 break
               }
             }
-            if ( new.pair[4] == "0" ) {
-              n.nonredundant.hit <- n.nonredundant.hit + 1
-              new.pair[4] = n.nonredundant.hit
+          }
+          if ( redundant ) next
+
+          # this pair is uniq, but maybe it belongs an existing isotope envelope pair
+          # the last digit is used to track unique isotope envelope pairs
+          new.pair <- pair.list.this.charge[F,]
+          #new.pair[1,] <- c( peak.index1, peak.index2, charge, max(peak.rt1, peak.rt2), 0 )
+          new.pair[1,] <- c( peak.index1, peak.index2, charge, peak.mz1, peak.mz2, peak.rt1, peak.rt2, 0 )
+          nrow.this.charge <- dim(pair.list.this.charge)[1]
+          if ( nrow.this.charge == 0 ) {
+            n.hit.this.charge <- n.hit.this.charge + 1
+            new.pair[1,"hit.id"] <- n.hit.this.charge
+            pair.list.this.charge <- rbind( pair.list.this.charge, new.pair )
+          } else {
+            for ( k in 1:nrow.this.charge) {
+              tmp.mz1 <- xpeaks[ pair.list.this.charge[k,"idx1"], "mz" ]
+              tmp.mz2 <- xpeaks[ pair.list.this.charge[k,"idx2"], "mz" ]
+              if (((tmp.mz2>peak.mz1)&(tmp.mz2<peak.mz2)) |
+                  ((tmp.mz1>peak.mz1)&(tmp.mz1<peak.mz2)) ) {
+                # this pair is from an existing isotopic envelope,
+                new.pair[1,"hit.id"] <- pair.list.this.charge[k,"hit.id"]
+                break
+              }
             }
-            pair.list.this.charge <- c( pair.list.this.charge, new.pair)
+            # this pair belongs to a new pair of isotopic envelopes
+            if ( new.pair[1,"hit.id"] == 0 ) {
+              n.hit.this.charge <- n.hit.this.charge + 1
+              new.pair[1,"hit.id"] = n.hit.this.charge
+            }
+            # add this pair to the list
+            pair.list.this.charge <- rbind( pair.list.this.charge, new.pair )
           }
         }
       }
     }
-    if ( length(pair.list) == 0 ) {
-      pair.list <- pair.list.this.charge
-    } else {
-      pair.list <- c( pair.list, pair.list.this.charge )
+    if (n.hit.this.charge == 0) next
+    pair.list.tmp <- pair.list.this.charge[F,]
+    for( k in 1:n.hit.this.charge ) {
+      pair.list.tmp <- pair.list.this.charge[pair.list.this.charge[,"hit.id"] == k, ]
+      valid <- sum( pair.list.tmp[,"rt1"]==rt | pair.list.tmp[,"rt2"]==rt)
+      if ( valid ) {
+        n.nonredundant.hit <- n.nonredundant.hit + 1
+        pair.list.tmp[,"hit.id"] <- n.nonredundant.hit
+        pair.list<- rbind( pair.list, pair.list.tmp )
+      }
     }
-    pair.list.this.charge <- NULL
+    #pair.list <- rbind( pair.list, pair.list.this.charge )
   }
 }
-if( length(pair.list) == 0 ) {
+cat("\n")
+
+if( dim(pair.list)[1] == 0 ) {
   print("no doublet peaks identified")
   quit()
 }
-pair.matrix <- matrix(pair.list, ncol=4, byrow=TRUE)
-colnames(pair.matrix) <- c("idx1", "idx2", "charge", "hit.id")
-pair.matrix <- data.frame( pair.matrix )
+# table with raw pair peak index, charge, rt and hit.id etc
+out.filename0 <- paste(output.path, output.file.base, ".pair_index.txt", sep="")
+write.table( format(pair.list,digits=10), out.filename0, quote=FALSE, row.names=FALSE)
 
+# filter and output
 rt.window.per.scan <- 6.0 # time elapse per ms1 scan
-mass.window <- 2.0 # plot 2.0 m/z unit from center of delected peak
+mass.window <- 2.0  # plot 2.0 m/z unit from center of delected peak
 num.ms2.per.ms1 <- 18
 out.table <- data.frame()
-for ( id in levels(factor(pair.matrix[,"hit.id"])) ) {
-  pair.matrix.this.id <- pair.matrix[ pair.matrix[,"hit.id"]==id, ]
-                                        # for peak intensity
-  peaks.maxo1 <- xpeaks[ as.character( pair.matrix.this.id[,"idx1"] ), "maxo"]
-  peaks.maxo2 <- xpeaks[ as.character( pair.matrix.this.id[,"idx2"] ), "maxo"]
+for ( id in levels(factor(pair.list[,"hit.id"])) ) {
+  pair.list.this.id <- pair.list[ pair.list[,"hit.id"]==id, ]
+  charge <- as.numeric(pair.list.this.id[1,"charge"])
+  isotope.mz.delta <- isotope.mass.unit / charge
+  # peak intensity
+  peaks.maxo1 <- xpeaks[ as.character( pair.list.this.id[,"idx1"] ), "maxo"]
+  peaks.maxo2 <- xpeaks[ as.character( pair.list.this.id[,"idx2"] ), "maxo"]
   peaks.maxo <- peaks.maxo1 + peaks.maxo2
-  peaks.maxo.cut <- peaks.maxo > max(peaks.maxo) * 0.01
+  peaks.maxo.cut <- peaks.maxo > max(peaks.maxo) * 0.1
   # within same isotopic cluster, get rid background noise by 1% cut
   peaks.maxo <- peaks.maxo[peaks.maxo.cut]
-  pair.matrix.this.id <- pair.matrix.this.id[peaks.maxo.cut,]
-  peaks.mz1 <- xpeaks[ as.character( pair.matrix.this.id[,"idx1"] ), "mz"]
-  peaks.mz2 <- xpeaks[ as.character( pair.matrix.this.id[,"idx2"] ), "mz"]
-                                        # for plot a specific ms1 scan
-  mass.range <- c( min(peaks.mz1) - mass.window, max(peaks.mz2) + mass.window )
-                                        # for peak intensity
-  #peaks.maxo1 <- xpeaks[ as.character( pair.matrix.this.id[,"idx1"] ), "maxo"]
-  #peaks.maxo2 <- xpeaks[ as.character( pair.matrix.this.id[,"idx2"] ), "maxo"]
-  #peaks.maxo <- peaks.maxo1 + peaks.maxo2
-                                        # num of peaks
-  peaks.n <- length( peaks.maxo )
-                                        # plot chromatogram of the most intense peak,
-                                        # label others under the same envelope
+  peaks.maxo1 <- peaks.maxo1[peaks.maxo.cut]
+  peaks.maxo2 <- peaks.maxo2[peaks.maxo.cut]
+  pair.list.this.id <- pair.list.this.id[peaks.maxo.cut,]
+  peaks.mz1 <- as.numeric(pair.list.this.id[,"mz1"])
+  peaks.mz2 <- as.numeric(pair.list.this.id[,"mz2"])
+  # most intense peak position
   i <- order(peaks.maxo)[length(peaks.maxo)]
-  peak.index1 <- as.character(pair.matrix.this.id[i,1])
-  peak.index2 <- as.character(pair.matrix.this.id[i,2])
-  charge <- as.numeric( as.character(pair.matrix.this.id[i,3]) )
+  envelope1 <- abs( (peaks.mz1-peaks.mz1[i])/isotope.mz.delta )
+  envelope2 <- abs( (peaks.mz2-peaks.mz2[i])/isotope.mz.delta )
+  envelope.check <- (abs(envelope1-round(envelope1))/peaks.mz1[i] < mz.ppm.cut &
+                     abs(envelope2-round(envelope2))/peaks.mz2[i] < mz.ppm.cut &
+                     round(envelope1) <= 4 &
+                     round(envelope2) <= 4)
+  pair.list.this.id <- pair.list.this.id[envelope.check,]
+  peaks.mz1 <- as.numeric(pair.list.this.id[,"mz1"])
+  peaks.mz2 <- as.numeric(pair.list.this.id[,"mz2"])
+  peaks.maxo <- peaks.maxo[envelope.check]
+  peaks.maxo1 <- peaks.maxo1[envelope.check]
+  peaks.maxo2 <- peaks.maxo2[envelope.check]
 
-  rt <- ( xpeaks[peak.index1,"rt"] + xpeaks[peak.index2,"rt"] ) / 2.0
+  # for plot a specific ms1 scan
+  mass.range <- c( min(peaks.mz1) - mass.window, max(peaks.mz2) + mass.window )
+  # num of peaks
+  peaks.n <- length( peaks.maxo )
+  # need at least three peak pairs per hit
+  if (peaks.n < 3) next
+  # plot chromatogram of the most intense peak,
+  # label others under the same envelope
+  i <- order(peaks.maxo)[length(peaks.maxo)]
+  rt <- ( as.numeric(pair.list.this.id[i,"rt1"]) + as.numeric(pair.list.this.id[i,"rt2"]) ) / 2.0
   rt.range <- c( rt-rt.window.per.scan, rt+rt.window.per.scan)
 
   raw.EIC.data <- rawEIC(xfile, massrange=mass.range, timerange=rt.range)
@@ -176,6 +241,9 @@ for ( id in levels(factor(pair.matrix[,"hit.id"])) ) {
   scan.data <- getScan(xfile, best.scan.number, massrange=mass.range)
   scan.mz <- scan.data[,"mz"]
   scan.int <- scan.data[,"intensity"]
+  scan.int.zoom <- scan.int[ scan.mz >= min(peaks.mz1) & scan.mz <= max(peaks.mz2) ]
+  # noisy peaks, skip
+  if ( max(peaks.maxo1,peaks.maxo2) != max(scan.int.zoom) ) next
   peak.mz1 <- peaks.mz1[i]
   peak.mz2 <- peaks.mz2[i]
   peak.maxo1 <- scan.int[ abs(scan.mz-peak.mz1) < mz.ppm.cut * peak.mz1 ]
@@ -186,7 +254,6 @@ for ( id in levels(factor(pair.matrix[,"hit.id"])) ) {
   if ( max(scan.int[ scan.mz>peak.mz1 & scan.mz<peak.mz2 ]) > 100 * max(peak.maxo1, peak.maxo2) )
     peaks.n <-0
                                         # a very simple way to find the monoisotopic peak
-  isotope.mz.delta <- isotope.mass.unit / charge
   lower.mz <- monoiso.mz <- peak.mz1
   lower.int <- monoiso.int <- peak.maxo1
   lower.int.ratio <- 1.0
@@ -197,8 +264,8 @@ for ( id in levels(factor(pair.matrix[,"hit.id"])) ) {
       lower.mz <- scan.mz[lower.mz.exist][1]
       lower.int <- scan.int[lower.mz.exist][1]
       lower.int.ratio <- lower.int/monoiso.int
-      if ( lower.int.ratio < 0.4 ) {
-                                        # intensity ratio drop too much
+      if ( lower.int.ratio < 0.2 ) {
+        # intensity ratio drop too much
         break
       } else {
         monoiso.mz <- lower.mz
@@ -226,7 +293,7 @@ for ( id in levels(factor(pair.matrix[,"hit.id"])) ) {
 
   out.filename <- paste(output.path, output.file.base, "_", peaks.n, "_", id, output.file.suffix, sep="")
                                         # make plot for each isotopic cluster
-    png(out.filename)
+  png(out.filename)
   par(mfrow=c(2,1))
   par(las=0)
                                         # mz spectrum top
@@ -242,14 +309,14 @@ for ( id in levels(factor(pair.matrix[,"hit.id"])) ) {
   ymark <- 0
                                         #label all identified peaks under this isotopic envelope
   points( peaks.mz1, rep(ymark, length(peaks.mz1) ), pch=23, col="red",bg="red")
-  points( peaks.mz2, rep(ymark, length(peaks.mz2) ), pch=23, col="blue",bg="blue")
+  points( peaks.mz2, rep(ymark, length(peaks.mz2) ), pch=24, col="blue",bg="blue")
   points( peak.mz1, peak.maxo1, pch="C", col="red",bg="red")
   points( peak.mz2, peak.maxo2, pch="C", col="blue",bg="blue")
   points( monoiso.mz, monoiso.int, pch="M", col="red",bg="red")
                                         #chromatogram bottom
   raw.ECI.light <- rawEIC(xfile, c(peak.mz1*(1-mz.ppm.cut), peak.mz1*(1+mz.ppm.cut)) )
   raw.ECI.heavy <- rawEIC(xfile, c(peak.mz2*(1-mz.ppm.cut), peak.mz2*(1+mz.ppm.cut)) )
-  xlimit <-c( max(1, best.scan.number-25), min(best.scan.number+25, length(raw.ECI.light[[1]]) ) )
+  xlimit <-c( max(1, best.scan.number-5), min(best.scan.number+5, length(raw.ECI.light[[1]]) ) )
   ylimit <- range(c(raw.ECI.light[[2]][xlimit[1]:xlimit[2]], raw.ECI.heavy[[2]][xlimit[1]:xlimit[2]]))
   plot(raw.ECI.light[[1]], raw.ECI.light[[2]], type="l", col="red",xlab="scan #", ylab="intensity",
        main=paste("Chromatogram of", as.character(format(peak.mz1, digits=7)),
@@ -257,8 +324,8 @@ for ( id in levels(factor(pair.matrix[,"hit.id"])) ) {
   lines(raw.ECI.heavy[[1]], raw.ECI.heavy[[2]], col='blue', xlim=xlimit, ylim=ylimit)
   dev.off()
                                         # save entries in a text table for output
-  outlist <- list( best.scan.number, charge, monoiso.mz*charge-Hplus.mass*charge, monoiso.mz, as.numeric(id), peaks.n )
-  names(outlist) <- c( "scan", "charge", "mass", "mz", "peaks.id", "peaks.count")
+  outlist <- list( best.scan.number, charge, monoiso.mz*charge-Hplus.mass*charge, monoiso.mz, peak.mz1, as.numeric(id), peaks.n )
+  names(outlist) <- c( "scan", "charge", "mono.mass", "mono.mz", "peak.mz", "peaks.id", "peaks.count")
   if ( length(out.table) == 0 ) {
     out.table <- data.frame( outlist )
   } else {
@@ -266,6 +333,7 @@ for ( id in levels(factor(pair.matrix[,"hit.id"])) ) {
   }
 }
 
+# table with peak.count
 rownames(out.table) <- out.table$peaks.id
 out.table <- out.table[ order(out.table$peaks.count, decreasing=TRUE), ]
 out.filename <- paste(output.path, output.file.base, ".table.txt", sep="")
@@ -293,20 +361,20 @@ image(massname, xfile@scantime[scanname], log(prof.matrix+1), col=gray(256:0/256
 title("XC/MS Log Intensity Image (whole)")
 col.max <- max( out.table[,"peaks.count"] )
 for (i in 1:dim(out.table)[1] ) {
-  xp <- round( out.table[i,"mz"] )
+  xp <- round( out.table[i,"mono.mz"] )
   yp <- out.table[i,"scan"]
   countp <- out.table[i,"peaks.count"]
   points( xp, xfile@scantime[yp], type="o", col = rainbow(col.max)[countp] )
 }
 
-x.lim <- round (range(out.table[,"mz"]) )
+x.lim <- round (range(out.table[,"mono.mz"]) )
 y.lim <- range(xfile@scantime[out.table[,"scan"]])
 image(massname, xfile@scantime[scanname], log(prof.matrix+1), col=gray(256:0/256),
       xlab="m/z", ylab="Seconds", zlim=log(range(xfile@env$intensity)), xlim=x.lim, ylim=y.lim )
 title("XC/MS Log Intensity Image (zoom)")
 col.max <- max( out.table[,"peaks.count"] )
 for (i in 1:dim(out.table)[1] ) {
-  xp <- round( out.table[i,"mz"] )
+  xp <- round( out.table[i,"mono.mz"] )
   yp <- out.table[i,"scan"]
   countp <- out.table[i,"peaks.count"]
   points( xp, xfile@scantime[yp], type="o", col = rainbow(col.max)[countp] )
