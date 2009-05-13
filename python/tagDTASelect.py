654fdb043f00c1b3776d3b5ca67c602caa916d3e
chuwang <chuwang@691117a8-ef3d-11dd-b559-41d664898b09>
2009-05-13 01:02:08 +0000

    tag a DTAselect file with IPI number in front of each peptide entry.

---
+++ python/tagDTASelect.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+#
+# tag each peptide line in DTASelect output file with its IPI name at beginning
+
+import sys
+from sys import argv
+
+if len(argv) != 2:
+    print 'Usage: %s <DTASelect-filter.txt>'%argv[0]
+    print 'tag each peptide line in DTASelect output file with its IPI name at beginning'
+    sys.exit(-1)
+
+# get current IPI tag
+tag=''
+for line in open(argv[1]):
+    line = line.rstrip()
+    words = line.split()
+    # none peptide entry line
+    if len(words) <= 5:
+        tag=''
+        print line
+        continue
+    if words[0].find('IPI:IPI') != -1:
+        i = words[0].find("|")
+        tag = words[0][0:i] if i else words[0]
+        print line
+    else:
+        print tag, line
+
+
+
