e750ab685765c44eaefbd5963f2e53c15d041e14
chuwang <chuwang@691117a8-ef3d-11dd-b559-41d664898b09>
2009-05-20 16:09:21 +0000

    1. add functions to identify chromatographic peaks and pairs given cerntain M/Z values. 2. switch from xcms::specPeaks function to my own customized function to detect chromtagraphic peaks 3. add elution profile linear regression panel and cofficient calculation 4. add output of text table with hyperlinks for easy EXCEL interface.

--- R/findMs1AcrossSets.R
+++ R/findMs1AcrossSets.R
@@ -22,34 +22,44 @@ Hplus.mass <- 1.0072765
 ## file name from input args
 args <- commandArgs(trailingOnly=T)
 ## the table with protein names
-original.table <- read.table(args[1],sep="\t", header=T)
+#original.table <- read.table(args[1],sep="\t", header=T)
 ## the table with mass and scan number from DTASelect
-cross.table <- read.table(args[2], header=T)
-cross.table[,"mass"] <- cross.table[,"mass"] + probe.mass
+#cross.table <- read.table(args[2], header=T)
+#cross.table[,"mass"] <- cross.table[,"mass"] + probe.mass
 ## file name tags
-cross.vec <- dimnames(cross.table)[[2]][-c(1,2)]
+#cross.vec <- dimnames(cross.table)[[2]][-c(1,2)]
 
 ## find all matched input files in current directory
 input.path <- getwd()
-#input.files <- list.files( path=input.path, pattern=paste(input.file.suffix,"$",sep="") )
-mzXML.names <- list.files(path="./",pattern="mzXML$")
-mzXML.files <- as.list( mzXML.names )
-names(mzXML.files) <- mzXML.names
-for (name in mzXML.names) {
-  cat(paste(name,"\n",sep=""))
-  mzXML.files[[name]] <- xcmsRaw( paste("./",name,sep=""), profstep=0, includeMSn=T)
-}
+#mzXML.names <- list.files(path="./",pattern="mzXML$")
+#mzXML.files <- as.list( mzXML.names )
+#names(mzXML.files) <- mzXML.names
+#for (name in mzXML.names) {
+#  cat(paste(name,"\n",sep=""))
+#  mzXML.files[[name]] <- xcmsRaw( paste("./",name,sep=""), profstep=0, includeMSn=T)
+#}
 
 
-# retention time window in secs
-rt.window <- 10
+## retention time window in secs
+rt.window <- 5
 rt.window.width <- rt.window * 60
-out.filename <- paste(output.path, "/", "output_rt_",as.character(rt.window),".ps", sep="")
+## signal/noise ratio for peak picking
+sn <- 2.5
+out.filename.base <- paste("output_rt_",as.character(rt.window),"_sn_",
+                      as.character(sn),sep="")
+out.filename <- paste(output.path,"/",out.filename.base,".ps",sep="")
 ## make plot for each isotopic cluster
 ##png(out.filename)
+out.df <- data.frame(entry=numeric(0), mass=numeric(0), charge=numeric(0), segment=character(0),
+                     ir.1.1=numeric(0), ir.1.5=numeric(0), ir.1.10=numeric(0),
+                     ar.1.1=numeric(0), ar.1.5=numeric(0), ar.1.10=numeric(0),link=character(0))
+original.df <- original.table[F,]
 postscript( out.filename, horizontal=F)
-par(mfrow=c(length(cross.vec),1), oma=c(0,0,5,0), las=0)
+layout(matrix(c(1,1,2,3,3,4,5,5,6), 3, 3, byrow = T))
+#par(mfrow=c(length(cross.vec),1))
+par(oma=c(0,0,5,0), las=0)
 
+all.count <- 0
 for ( i in 1:dim(cross.table)[1] ) {
   peptide <- cross.table[i,1]
   mono.mass  <- cross.table[i,"mass"]
@@ -69,7 +79,8 @@ for ( i in 1:dim(cross.table)[1] ) {
   count <- 0
   for ( f in levels(as.factor(raw.file.charge))  ) {
     if (f == "none.none") next
-      count <- count+1
+    count <- count+1
+    all.count <- all.count + 1
     tmp.vec <- unlist( strsplit(f,"\\.") )
     raw.index <- tmp.vec[1]
     charge <- as.integer(tmp.vec[2])
@@ -85,14 +96,20 @@ for ( i in 1:dim(cross.table)[1] ) {
       ms1.scan.num[k] <- which(xfile@acquisitionNum > as.integer(raw.scan.num[kk]))[1]-1
     }
 
+    ratios <- rep(0,length(cross.vec))
     for ( j in 1:length(cross.vec) ) {
-      if ( j %in% exist.index ) {
-        tag = "*"
-      } else {
-        tag = ""
-      }
       raw.file <- paste( cross.vec[j], "_", raw.index,".mzXML",sep="")
       xfile <- mzXML.files[[raw.file]]
+      # tag * and tag rt line
+      if ( j %in% exist.index ) {
+        tag <- "*"
+        tag.ms1.scan.num <- ms1.scan.num[match(j,exist.index)]
+        tag.rt <- xfile@scantime[tag.ms1.scan.num]/60
+      } else {
+        tag <- ""
+        tag.ms1.scan.num <- NA
+        tag.rt <- NA
+      }
       ##chromatogram bottom
       raw.ECI.light <- rawEIC(xfile, c(mz.light*(1-mz.ppm.cut), mz.light*(1+mz.ppm.cut)) )
       raw.ECI.heavy <- rawEIC(xfile, c(mz.heavy*(1-mz.ppm.cut), mz.heavy*(1+mz.ppm.cut)) )
@@ -118,22 +135,117 @@ for ( i in 1:dim(cross.table)[1] ) {
       xlimit <- c(rt.min,rt.max)/60
       raw.ECI.light.rt <- xfile@scantime[ raw.ECI.light[[1]] ] / 60
       raw.ECI.heavy.rt <- xfile@scantime[ raw.ECI.heavy[[1]] ] / 60
+      ##if ( tag=="*") {
+      tt.main <- paste(tag, raw.file, "; Census ratio:", format(original.table[i,j+3], digits=4),
+                       "; Raw Scan:", as.character(raw.scan.num[j]))
+      ##} else {
+      ##  tt.main <- paste(tag, raw.file, "; Census ratio: NA; Raw Scan: NA")
+      ##}
       plot(raw.ECI.light.rt, raw.ECI.light[[2]], type="l", col="red",xlab="Retention Time(min)",
-           ylab="intensity",
-           main=paste(tag, raw.file, "; Census ratio:", as.character(original.table[i,j+3]),
-             "; Raw Scan:", as.character(raw.scan.num[j])),
-           xlim=xlimit,ylim=ylimit)
+           ylab="intensity", main=tt.main, xlim=xlimit,ylim=ylimit)
       lines(raw.ECI.heavy.rt, raw.ECI.heavy[[2]], col='blue', xlim=xlimit, ylim=ylimit)
-                                        #rt.tick <- round(seq(from=xlimit[1],to=xlimit[2], length=5))
-                                        #axis(3, at=rt.tick, labels=as.character(format((xfile@scantime[rt.tick])/60,digits=7) ) )
+      if ( !is.na(tag.rt) ) {
+        #lines(c(tag.rt,tag.rt),c(0.0, max(raw.ECI.light[[2]],raw.ECI.heavy[[2]])), col="green")
+        points(tag.rt, raw.ECI.light[[2]][tag.ms1.scan.num], type='p',pch=8,col="red")
+      }
+      ## guess ratio of integrated peak area
+      peaks <- findPairChromPeaks( raw.ECI.light.rt, raw.ECI.light[[2]], raw.ECI.heavy[[2]],xlimit,sn )
+      noise.light <- peaks[1]
+      lines(xlimit,c(noise.light, noise.light), col='red', type='l', lty=2)
+      noise.heavy <- peaks[2]
+      lines(xlimit,c(noise.heavy, noise.heavy), col='blue', type='l', lty=2)
+      peaks <- peaks[-c(1,2)]
+      n.peaks <- length(peaks)/2
+
+      best.r2 <- best.npoints <- best.ratio <- 0.0
+      best.xlm <- best.light.yes <- best.heavy.yes <- best.low <- best.high <- NULL
+      best.fixed <- F
+      if (n.peaks>0) {
+        for (n in 1:n.peaks) {
+          low <- peaks[2*n-1]
+          high<- peaks[2*n]
+          yes <- which( raw.ECI.light.rt>=low & raw.ECI.light.rt<=high )
+          light.yes <- raw.ECI.light[[2]][yes]
+          heavy.yes <- raw.ECI.heavy[[2]][yes]
+          ## calculate ratio of integrated peak area
+          ratio <- sum(light.yes)/sum(heavy.yes)
+          if (ratio > 15 ) next
+          lines(c(low,low),ylimit/10, col="green")
+          lines(c(high,high),ylimit/10, col="green")
+          text(mean(c(low,high)),max(light.yes,heavy.yes),
+               labels=format(ratio,digits=4))
+          ## calculate peak co-elution profile using only points above noise line
+          yes2 <- light.yes > noise.light & heavy.yes > noise.heavy
+          light.yes <- light.yes[yes2]
+          heavy.yes <- heavy.yes[yes2]
+          npoints <- length(light.yes)
+          if ( npoints<2 ) {
+            next
+          } else if ( npoints<3) {
+            light.yes <- c(1,light.yes)
+            heavy.yes <- c(1,heavy.yes)
+          }
+          x.lm <- lsfit( x=log10(light.yes), y=log10(heavy.yes) )
+          r2 <- as.numeric(ls.print(x.lm,print.it=F)$summary[1,2])
+          if ( !is.na(tag.rt) & tag.rt>=low & tag.rt<=high) {
+            best.fixed <- T
+          }
+          if ( best.fixed | npoints > best.npoints ) {
+            best.npoints <- npoints
+            best.r2 <- r2
+            best.ratio <- ratio
+            best.xlm <- as.numeric(ls.print(x.lm,print.it=F)$coef.table[[1]][,"Estimate"])
+            best.low <- low
+            best.high <- high
+            best.light.yes <- light.yes
+            best.heavy.yes <- heavy.yes
+          }
+          if (best.fixed) break
+        }
+      }
+      if ( best.r2 != 0 ) {
+        lines(c(best.low,best.low),ylimit, col="green")
+        lines(c(best.high,best.high),ylimit, col="green")
+        plot(log10(best.light.yes),log10(best.heavy.yes),
+             xlab="log10(intensity.light)", ylab="log10(intensity.heavy)",
+             main=paste("R2 value:",format(best.r2,digits=3)),
+             xlim=range(log10(best.light.yes),log10(best.heavy.yes)),
+             ylim=range(log10(best.light.yes),log10(best.heavy.yes)))
+        abline(best.xlm[1],best.xlm[2])
+        abline(0,1,col="grey")
+        ratios[j] <- best.ratio
+      } else {
+        plot(0,0,xlab="",ylab="",main=paste("R2 value: 0.00") )
+      }
     }
     tt <- paste("Entry ", as.character(i), "-  Charge: ", as.character(charge),
                 " - M/Z: ", as.character(format(mz.light, digits=7)),
                 "and", as.character(format(mz.heavy,digits=7)))
     mtext(tt, line=3.5, outer=T)
     mtext(paste(original.table[i,"Peptide_Sequence"],"; Mono.mass: ", as.character(mono.mass), sep=""),
-         cex=0.8, line=2, out=T)
+          cex=0.8, line=2, out=T)
     mtext(substr(original.table[i,"Description"], 1,100), line=1, cex=0.5,out=T)
+    ## save data in outdf
+    original.df <- rbind( original.df, original.table[i,] )
+    this.df <- data.frame(entry=i, mass=mass, charge=charge, segment=raw.index,
+                          ir.1.1=ratios[1], ir.1.5=ratios[2], ir.1.10=ratios[3],
+                          ar.1.1=0, ar.1.5=0, ar.1.10=0,
+                          link=paste('=HYPERLINK(\"./',out.filename.base,'-',as.character(all.count-1),'.png\")',sep=''))
+    out.df <- rbind(out.df, this.df)
   }
 } ## each entry
 dev.off()
+
+for (k in levels(as.factor(out.df[,"entry"])) ) {
+  kk <- which(as.factor(out.df[,"entry"]) == k )
+  for ( m in 1:length(cross.vec) ) {
+    v <- out.df[kk,m+4]
+    v <- v[v>0]
+    out.df[kk,m+7] <- mean(v)
+  }
+}
+
+all.table <- cbind(original.df,out.df)
+row.names(all.table) <- as.character(seq(1:dim(all.table)[1]) )
+write.table(all.table,file=paste(output.path,"/",out.filename.base,".txt",sep=""), quote=F, sep="\t", row.names=F)
+
--- R/msisotope.R
+++ R/msisotope.R
@@ -68,3 +68,102 @@ find.ms2.triggered <- function(xfile, yfile, predicted.mz, rt.range) {
   }
   return(ms2.matrix)
 }
+##
+estimateChromPeakNoise <- function(intensity) {
+  mean(intensity)
+}
+
+findChromPeaks <- function(spec, sn=5, rtgap=0.2 ) {
+
+  noise <- estimateChromPeakNoise(spec[,"intensity"] )
+
+  spectab <- matrix(nrow = 0, ncol = 4)
+  colnames(spectab) <- c("rt", "rt.min", "rt.max", "sn")
+
+  while (spec[i <- which.max(spec[,"intensity"]), "intensity"] > noise*sn) {
+
+    rt <- spec[i,"rt"]
+    intensity <- spec[i,"intensity"]
+    rt.range <- xcms:::descendValue(spec[,"intensity"], noise, i)
+    rt.range <- c(max(1,rt.range[1]-1), min(nrow(spec),rt.range[2]+1) )
+
+    if (rt.range[1] >= 1 && rt.range[2] <= nrow(spec)) {
+      rt.min <- spec[rt.range[1],"rt"]
+      rt.max <- spec[rt.range[2],"rt"]
+      if (!any(abs(spectab[,"rt"] - rt) <= rtgap))
+        spectab <- rbind(spectab, c(rt, rt.min, rt.max, spec[i,"intensity"]/noise))
+    }
+    spec[seq(rt.range[1], rt.range[2]),"intensity"] <- 0
+    }
+
+    spectab
+}
+
+findPairChromPeaks <- function(rt, light.int, heavy.int, rt.range, sn=5) {
+  rtdiff <- mean( diff(rt) )
+
+  m.light <- cbind(rt,light.int)
+  dimnames(m.light)[[2]] <- c("rt","intensity")
+  m.light <- m.light[rt>=rt.range[1]&rt<=rt.range[2],]
+  noise.light <- estimateChromPeakNoise(m.light[,"intensity"])
+  peaks.light <- findChromPeaks(m.light,sn,rtgap=0.5)
+  n.light <- dim(peaks.light)[[1]]
+
+  m.heavy <- cbind(rt,heavy.int)
+  dimnames(m.heavy)[[2]] <- c("rt","intensity")
+  m.heavy <- m.heavy[rt>=rt.range[1]&rt<=rt.range[2],]
+  noise.heavy <- estimateChromPeakNoise(m.heavy[,"intensity"])
+  peaks.heavy <- findChromPeaks(m.heavy,sn,rtgap=5*rtdiff)
+  n.heavy <- dim(peaks.heavy)[[1]]
+
+  pair.range <- c(noise.light, noise.heavy)
+  if ( n.heavy == 0 | n.light == 0 ) return(pair.range)
+
+  for (i in 1:n.light) {
+    rt.i <- peaks.light[i,"rt"]
+    d.rt <- abs(rt.i-peaks.heavy[,"rt"])
+    j <- which.min(d.rt)
+   # if (d.rt[j]>5*rtdiff) next
+    rt.j <- peaks.heavy[j,"rt"]
+    if ( rt.j >=peaks.light[i,"rt.min"] & rt.j <= peaks.light[i,"rt.max"]
+        &  rt.i >=peaks.heavy[j,"rt.min"] & rt.i <= peaks.heavy[j,"rt.max"] ) {
+      low <- min(peaks.light[i,"rt.min"],peaks.heavy[j,"rt.min"])
+      high <- max(peaks.light[i,"rt.max"],peaks.heavy[j,"rt.max"])
+      if ( low < high ) {
+        pair.range <- c(pair.range,low,high)
+      }
+    }
+  }
+  return(pair.range)
+}
+
+#findPairChromPeaks <- function(rt, light.int, heavy.int, rt.range) {
+#  m.light <- cbind(rt,light.int)
+#  dimnames(m.light)[[2]] <- c("mz","intensity")
+#  m.light <- m.light[rt>=rt.range[1]&rt<=rt.range[2],]
+#  noise.light <- specNoise(m.light)
+#  peaks.light <- specPeaks(m.light,sn=10,mzgap=0.5)
+#  n.light <- dim(peaks.light)[[1]]
+
+#  m.heavy <- cbind(rt,heavy.int)
+#  dimnames(m.heavy)[[2]] <- c("mz","intensity")
+#  m.heavy <- m.heavy[rt>=rt.range[1]&rt<=rt.range[2],]
+#  noise.heavy <- specNoise(m.heavy)
+#  peaks.heavy <- specPeaks(m.heavy,sn=10,mzgap=0.5)
+#  n.heavy <- dim(peaks.heavy)[[1]]
+
+#  pair.range <- integer(0)
+#  if ( n.heavy == 0 | n.light == 0 ) return(pair.range)
+
+#  for (i in 1:n.light) {
+#    rt.i <- peaks.light[i,"mz"]
+#    d.rt <- abs(rt.i-peaks.heavy[,"mz"])
+#    j <- which.min(d.rt)
+#    if (d.rt[j]>0.5) next
+#    rt.j <- peaks.heavy[j,"mz"]
+#    low <- min(rt.i-1.2*peaks.light[i,"fwhm"],rt.j-1.2*peaks.heavy[j,"fwhm"])
+#    high <- max(rt.i+1.2*peaks.light[i,"fwhm"],rt.j+1.2*peaks.heavy[j,"fwhm"])
+#    pair.range <- c(pair.range,low,high)
+#  }
+#  return(pair.range)
+#}
