1da1f10b1015ed6dad7b2fdae0bde0a98afce87a
chuwang <chuwang@691117a8-ef3d-11dd-b559-41d664898b09>
2009-11-18 01:58:53 +0000

    a small fix to allow by_protein option to be read.

--- R/runCombined.bash
+++ R/runCombined.bash
@@ -22,7 +22,7 @@ do
 done
 dirs=$(echo $allpt | sed "s/$txt//g")
 
-if [ $by_protein == "by_protein" ]; then
+if [ "$by_protein" == "by_protein" ]; then
     R --vanilla --args $txt $dirs < /home/chuwang/svnrepos/R/combined_by_protein.R > $outname.by_protein.Rout
 else
     R --vanilla --args $txt $dirs < /home/chuwang/svnrepos/R/combined.R > $outname.Rout
