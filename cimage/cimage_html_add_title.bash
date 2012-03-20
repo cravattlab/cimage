0d1151b694684feaefd942bf0d4b30317593c575
chuwang <chuwang@691117a8-ef3d-11dd-b559-41d664898b09>
2012-03-20 23:11:18 +0000

    nothing but a test

--- cimage/cimage_html_add_title.bash
+++ cimage/cimage_html_add_title.bash
@@ -5,9 +5,9 @@ if [ $# -lt 2 ]; then
     exit -1
 fi
 
+
 file=$1
 title=$2
 
 cat $file | sed 's/<TITLE>\w*<\/TITLE>//g' | sed "s/<HEAD>/<HEAD><TITLE>$title<\/TITLE>/g" > $file.tmp
 mv $file.tmp $file
-
